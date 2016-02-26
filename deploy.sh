56c2b81460e995025ac6060fd9d41ba554e71b9c
Dave Cassel <dmcassel@gmail.com>
2016-02-26 16:32:50 -0500

    adding a script to deploy individual modules

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+# Use this to deploy source code to the modules database.
+# 
+# Usage:
+#   deploy.sh <filename to deploy> <uri in modules database>
+# Example
+# ./deploy.sh unit-01/corb/uris.xqy /corb/uris.xqy
+#   or from the unit-01 directory
+# ../deploy.sh corb/uris.xqy /corb/uris.xqy
+
+USERNAME="admin" #update if required
+PASSWORD="admin" #update if required
+
+echo Deploying $1 to $2
+
+URI=$2
+
+curl --anyauth --user $USERNAME:$PASSWORD -X PUT -d@$1 \
+  -i -H "Content-type: application/xquery" \
+  "http://localhost:8080/v1/documents?database=employees-modules&uri=$URI"
