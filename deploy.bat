265f8756a40eb022c7f71cafaad709002889d2d6
wpaven <wpaven@yahoo.com>
2016-05-05 13:12:21 -0400

    update to --data-binary

--- deploy.bat
+++ deploy.bat
@@ -15,6 +15,9 @@ SET ARG1=%1
 
 echo "Deploying" %ARG1%
 
-curl --anyauth --user %USERNAME%:%PASSWORD% -X PUT -d@%ARG1% -i -H "Content-type: application/vnd.marklogic-xdmp" "http://localhost:8080/v1/ext/corb/%ARG1%?perm:rest-reader=read&perm:rest-writer=update&perm:modeler=execute"
+REM Update below to deploy either XQuery or JavaScript as required.
+REM application/vnd.marklogic-javascript
+
+curl --anyauth --user %USERNAME%:%PASSWORD% -X PUT --data-binary @%ARG1% -i -H "Content-type: application/vnd.marklogic-xdmp" "http://localhost:8080/v1/ext/corb/%ARG1%?perm:rest-reader=read&perm:rest-writer=update&perm:modeler=execute"
 
 
