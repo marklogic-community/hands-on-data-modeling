5bdb8b06c1017acb9669e1324d5f4c78ed162554
Dave Cassel <dmcassel@gmail.com>
2016-05-24 14:42:46 -0400

    #70 updating master with latest deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,16 @@
 # Use this to deploy source code to the modules database.
 #
 # Usage:
-#   deploy.sh <filename to deploy> <uri in modules database>
+#   deploy.sh <filename to deploy>
 # Example
-# ./deploy.sh unit-01/corb/uris.xqy /corb/uris.xqy
-#   or from the unit-01 directory
-# ../deploy.sh corb/uris.xqy /corb/uris.xqy
+# cd unit-01/corb
+# ../../deploy.sh 01-uris.xqy
 
-USERNAME="admin" #update if required
-PASSWORD="admin" #update if required
+USERNAME="dr-who" #update if required
+PASSWORD="doctor" #update if required
 
-echo Deploying $1 to $2
-
-URI=$2
+echo Deploying $1
 
 curl --anyauth --user $USERNAME:$PASSWORD -X PUT --data-binary @$1 \
   -i -H "Content-type: application/xquery" \
-  "http://localhost:8080/v1/documents?database=employees-modules&uri=$URI"
+  "http://localhost:8080/v1/ext/corb/$1?perm:rest-reader=read&perm:rest-writer=update&perm:modeler=execute"
